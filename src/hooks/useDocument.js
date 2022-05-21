0743db1b0791fc0b820a5dd8fc0895460475aff3
robonexx <robertwagar@gmail.com>
2022-05-21 15:46:17 +0200

    new hook for fetching documents

---
+++ src/hooks/useDocument.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { projectFirestore } from '../firebase/config';
+
+export const useDocument = (collection, id) => {
+  const [document, setDocument] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const ref = projectFirestore.collection(collection).doc(id);
+
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        setDocument({ ...snapshot.data(), id: snapshot.id });
+        setError(null);
+      },
+      (err) => {
+        console.log(err.message);
+        setError('Failed to get document');
+      }
+    );
+
+    return () => unsubscribe();
+  }, [collection, id]);
+
+  return { document, error };
+};
